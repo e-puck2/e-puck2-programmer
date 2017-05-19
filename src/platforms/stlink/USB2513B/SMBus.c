884427a81ab13407fd4effc8ee99c9b21e6e3b2a
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-05-19 10:27:08 +0200

    - Corrected SMBus function and added brief

--- src/platforms/stlink/USB2513B/SMBus.c
+++ src/platforms/stlink/USB2513B/SMBus.c
@@ -1,17 +1,46 @@
-//exemples https://github.com/libopencm3/libopencm3-examples/tree/master/examples/stm32/f1/other/i2c_stts75_sensor
-//http://www.longlandclan.yi.org/~stuartl/stm32f10x_stdperiph_lib_um/stm32f10x__i2c_8c_source.html
-//https://github.com/Selat/stm32-smbus
+/*
+Exemples 
+https://github.com/libopencm3/libopencm3-examples/tree/master/examples/stm32/f1/other/i2c_stts75_sensor
+http://www.longlandclan.yi.org/~stuartl/stm32f10x_stdperiph_lib_um/stm32f10x__i2c_8c_source.html
+https://github.com/Selat/stm32-smbus
+*/
+
+/**
+ * @file    SMBus.h
+ * @brief   Functions to configure SMBus host over I2C1
+ * 
+ * @author  Eliot Ferragni
+ */
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/i2c.h>
 #include <USB2513B/SMBus.h>
 
+////////////// PARAMETERS ////////////////
+#define I2C_SMBUS_HOST_MODE     0x000A
 
+//////////////////// PROTOTYPES PRIVATE FUNCTIONS /////////////////////
+
+void i2c_smbus_host_mode(uint32_t i2c);
+
+//////////////////// PRIVATE FUNCTIONS /////////////////////////
+
+/**
+ * @brief [Set the SMBus host mode]
+ * 
+ * @param i2c [I2C interface to configure]
+ */
+void i2c_smbus_host_mode(uint32_t i2c)
+{
+    I2C_CR1(i2c) |= I2C_SMBUS_HOST_MODE;
+}
+
+
+//////////////////// PUBLIC FUNCTIONS /////////////////////////
 void SMBus_init()
 {
-    /* Enable clocks for I2C2 and AFIO. */
+    /* Enable clocks for I2C1 */
     rcc_periph_clock_enable(RCC_I2C1);
-    rcc_periph_clock_enable(RCC_AFIO);
 
     /* Enable GPIOB clock. */
     rcc_periph_clock_enable(RCC_GPIOB);
@@ -31,10 +60,11 @@ void SMBus_init()
     /* 100KHz - I2C Standard Mode */
     i2c_set_standard_mode(I2C1);
 
+    /*SMBus Host mode*/
     i2c_smbus_host_mode(I2C1);
 
     /*
-     * fclock for I2C is 36MHz APB2 and ccr = fclock/(i2c_speed * 2) = 180 =>0xB4
+     * fclock for I2C is 36MHz APB1 and ccr = fclock/(i2c_speed * 2) = 180 =>0xB4
      */
     i2c_set_ccr(I2C2, 0xB4);
 
@@ -49,109 +79,99 @@ void SMBus_init()
     i2c_peripheral_enable(I2C1);
 }
 
-void i2c_smbus_host_mode(uint32_t i2c)
-{
-    I2C_CR1(i2c) |= I2C_SMBUS_HOST_MODE;
-}
-
-void SMBus_write(uint32_t i2c, uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas)
+void SMBus_write(uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas)
 {
     if(nbDatas > 0){
         uint32_t reg32 __attribute__((unused));
 
         /* Send START condition. */
-        i2c_send_start(i2c);
+        i2c_send_start(I2C1);
 
         /* Waiting for START is send and switched to master mode. */
-        while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-            & (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+        while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+            & (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
 
         /* Send destination address. */
-        i2c_send_7bit_address(i2c, i2c_addr>>1, I2C_WRITE);
+        i2c_send_7bit_address(I2C1, i2c_addr>>1, I2C_WRITE);
 
         /* Waiting for address is transferred. */
-        while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
+        while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
 
         /* Cleaning ADDR condition sequence. */
-        reg32 = I2C_SR2(i2c);
+        reg32 = I2C_SR2(I2C1);
 
-        i2c_send_data(i2c, reg); /* register */
-        while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+        i2c_send_data(I2C1, reg); /* register */
+        while (!(I2C_SR1(I2C1) & (I2C_SR1_BTF | I2C_SR1_TxE)));
 
         /* Sending the data. */
         do{
-            i2c_send_data(i2c, *datas++); 
-            while (!(I2C_SR1(i2c) & I2C_SR1_BTF)); /* Await ByteTransferedFlag. */
+            i2c_send_data(I2C1, *datas++); 
+            while (!(I2C_SR1(I2C1) & I2C_SR1_BTF)); /* Await ByteTransferedFlag. */
             nbDatas--;
         }while(nbDatas > 0);
         
         /* Send STOP condition. */
-        i2c_send_stop(i2c);
+        i2c_send_stop(I2C1);
     }
 }
 
-void SMBus_read(uint32_t i2c, uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas)
+void SMBus_read(uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas)
 {
     if(nbDatas > 0){
         uint32_t reg32 __attribute__((unused));
 
         /* Send START condition. */
-        i2c_send_start(i2c);
+        i2c_send_start(I2C1);
 
         /* Waiting for START is send and switched to master mode. */
-        while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-            & (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+        while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+            & (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
 
         /* Say to which address we want to talk to. */
-        i2c_send_7bit_address(i2c, i2c_addr>>1, I2C_WRITE);
+        i2c_send_7bit_address(I2C1, i2c_addr>>1, I2C_WRITE);
 
         /* Waiting for address is transferred. */
-        while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
+        while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
 
         /* Cleaning ADDR condition sequence. */
-        reg32 = I2C_SR2(i2c);
+        reg32 = I2C_SR2(I2C1);
 
-        i2c_send_data(i2c, reg); /* register */
-        while (!(I2C_SR1(i2c) & (I2C_SR1_BTF | I2C_SR1_TxE)));
+        i2c_send_data(I2C1, reg); /* register */
+        while (!(I2C_SR1(I2C1) & (I2C_SR1_BTF | I2C_SR1_TxE)));
 
         /* Send START condition. */
-        i2c_send_start(i2c);
+        i2c_send_start(I2C1);
 
         /* Waiting for START is send and switched to master mode. */
-        while (!((I2C_SR1(i2c) & I2C_SR1_SB)
-            & (I2C_SR2(i2c) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+        while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+            & (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
 
         /* Say to what address we want to talk to. */
-        i2c_send_7bit_address(i2c, i2c_addr>>1, I2C_READ); 
+        i2c_send_7bit_address(I2C1, i2c_addr>>1, I2C_READ); 
 
         /* Waiting for address is transferred. */
-        while (!(I2C_SR1(i2c) & I2C_SR1_ADDR));
+        while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
 
         /* Cleaning ADDR condition sequence. */
-        reg32 = I2C_SR2(i2c);
+        reg32 = I2C_SR2(I2C1);
 
-        if(nbDatas == 1){
-            //Deactivate ack
-            I2C_CR1(i2c) &= ~I2C_CR1_ACK;
-
-        }else{
-            //activate ack
-            I2C_CR1(i2c) |= I2C_CR1_ACK;
-        }
+        //activate ack
+        i2c_enable_ack(I2C1);
 
         //read until the N-1 byte
-        do{
+        while(nbDatas > 1)
+        {
              /* Now the slave should begin to send us the first byte. Await BTF. */
-            while (!(I2C_SR1(i2c) & I2C_SR1_BTF));
-            *datas++ = I2C_DR(i2c); /* MSB */
+            while (!(I2C_SR1(I2C1) & I2C_SR1_BTF));
+            *datas++ = i2c_get_data(I2C1);
             nbDatas--;
-        }while(nbDatas > 1);
+        }
 
-        I2C_CR1(i2c) &= ~I2C_CR1_ACK;
-       
-        //read the last byte
-        I2C_CR1(i2c) |= I2C_CR1_STOP;
-
-        *datas++ = I2C_DR(i2c);
+        //Deactivate ack
+        i2c_disable_ack(I2C1);
+        /* Send STOP condition. */
+        i2c_send_stop(I2C1);
+        //Read the last Byte
+        *datas = i2c_get_data(I2C1);
     }
 }
--- src/platforms/stlink/USB2513B/SMBus.h
+++ src/platforms/stlink/USB2513B/SMBus.h
@@ -1,10 +1,38 @@
+/**
+ * @file    SMBus.h
+ * @brief   Functions to configure SMBus host over I2C1
+ * 
+ * @author  Eliot Ferragni
+ */
+
+
+
 #include "general.h"
 
-#define I2C_SMBUS_HOST_MODE		0x000A
 
+//////////////////// PROTOTYPES PUBLIC FUNCTIONS /////////////////////
 
-
+/**
+ * @brief [Init the SMBus interface with I2C1]
+ */
 void SMBus_init(void);
-void i2c_smbus_host_mode(uint32_t i2c);
-void SMBus_write(uint32_t i2c, uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas);
-void SMBus_read(uint32_t i2c, uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas);
+/**
+ * @brief [SMBus write functions]
+ * @details [Transmit data through SMBus protocole]
+ * 
+ * @param i2c_addr [I2C addresse in 8bit of the peripherical to talk to]
+ * @param reg [Address of the register to access]
+ * @param nbDatas [Number of datas to transmit in Byte]
+ * @param datas [Pointer to the datas to transmit]
+ */
+void SMBus_write(uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas);
+/**
+ * @brief [SMBus read functions]
+ * @details [Receive datas through SMBus protocole]
+ * 
+ * @param i2c_addr [I2C addresse in 8bit of the peripherical to talk to]
+ * @param reg [Address of the register to access]
+ * @param nbDatas [Number of datas to receive in Byte]
+ * @param datas [Pointer to save the datas]
+ */
+void SMBus_read(uint8_t i2c_addr, uint8_t reg, uint8_t nbDatas, uint8_t* datas);
