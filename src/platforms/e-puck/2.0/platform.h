eb1a4630068017a5110154bcb0c3f8bcd154702f
dburnier <danielburnier@gmail.com>
2017-08-18 10:13:50 +0200

    Fix LED's implementation.

--- src/platforms/e-puck/2.0/platform.h
+++ src/platforms/e-puck/2.0/platform.h
@@ -205,8 +205,8 @@
 } while(0)
 
 #define SET_RUN_STATE(state)	{running_status = (state);}
-#define SET_IDLE_STATE(state)	{gpio_set_val(LED_PORT, LED_IDLE_RUN, state);}
-#define SET_ERROR_STATE(state)	{gpio_set_val(LED_PORT, LED_ERROR, state);}
+#define SET_IDLE_STATE(state)	{gpio_set_val(LED_PORT, LED_IDLE_RUN, !state);}
+#define SET_ERROR_STATE(state)	{gpio_set_val(LED_PORT, LED_ERROR, !state);}
 
 static inline int platform_hwversion(void)
 {
