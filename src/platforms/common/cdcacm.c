0b38d7dbc67c4e1a54d44db1de3daed00a6a9cec
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-10-17 16:34:49 +0200

    - Corrected compilation error

--- src/platforms/common/cdcacm.c
+++ src/platforms/common/cdcacm.c
@@ -471,9 +471,10 @@ static int cdcacm_control_request(usbd_device *dev,
 			return 1;
 #if !defined(PLATFORM_HAS_NO_SERIAL)
 		case SERIAL_COMM_IFACE_NUM:
+			
+#if defined(EPUCK2)
 			cdcacm_uart_dtr = (req->wValue & (1<<0) ? 1 : 0);
 			cdcacm_uart_rts = (req->wValue & (1<<1) ? 1 : 0);
-#if defined(EPUCK2)
 			gpio_set_val(EN_ESP32_PORT, EN_ESP32_PIN, !cdcacm_uart_rts || cdcacm_uart_dtr);
 			gpio_set_val(GPIO0_ESP32_PORT, GPIO0_ESP32_PIN, cdcacm_uart_rts || !cdcacm_uart_dtr);
 #endif
