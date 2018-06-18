3a64f47620052dc9e26c02af263647dc34e1e4c7
Eliot Ferragni <eliot.ferragni@epfl.ch>
2018-06-18 16:29:11 +0200

    -Added Vbus_detection to configure the USB Hub each time we connect the cable

--- src/platforms/e-puck/2.0/platform.c
+++ src/platforms/e-puck/2.0/platform.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "ch.h"
-#include "hal.h"
+#include <ch.h>
+#include <hal.h>
 
-#include "shell.h"
-#include "chprintf.h"
+#include <shell.h>
+#include <chprintf.h>
 
-#include "usbcfg.h"
+#include <platform.h>
+
+#include <usbcfg.h>
 #include <i2c_smbus.h>
 #include <USB251XB.h>
 
@@ -23,6 +25,41 @@ void panic_handler(const char *reason)
 
     }
 }
+
+static THD_WORKING_AREA(usb_hub_thd_wa, 128);
+static THD_FUNCTION(usb_hub_thd, arg)
+{
+  (void) arg;
+  bool hub_state = NOT_CONFIGURED;
+
+  /* Enabling events on both edges of the button line.*/
+  palEnableLineEvent(LINE_VBUS_DET, PAL_EVENT_MODE_BOTH_EDGES);
+  //init the first time the hub
+  USB251XB_init(USB2512B);
+  hub_state = CONFIGURED;
+
+  while(1){
+    //waiting until an event on the line is detected
+    palWaitLineTimeout(LINE_VBUS_DET, TIME_INFINITE);
+    //wait a few moments to be sure the interruption was not triggered
+    //by a glitch and then test the GPIO
+    //also wait for the USB HUB to be running
+    chThdSleepMilliseconds(DEBOUNCE_TIME_VBUS_DET_MS);
+
+    if(palReadLine(LINE_VBUS_DET)){
+      if(hub_state == NOT_CONFIGURED){
+        USB251XB_init(USB2512B);
+        hub_state = CONFIGURED;
+      }
+      
+    }else{
+      hub_state = NOT_CONFIGURED;
+    }
+  }
+}
+
+
+
 static THD_WORKING_AREA(test_thd_wa, 256);
 static THD_FUNCTION(test_thd, arg)
 {
@@ -55,9 +92,12 @@ int main(void) {
   halInit();
   chSysInit();
 
+  //SMBUS init
   i2c_smbus_start();
-  USB251XB_init(USB2512B);
 
+  //Vbus detection init. Used to configure the USB Hub when we detect an USB cable
+  chThdCreateStatic(usb_hub_thd_wa, sizeof(usb_hub_thd_wa), NORMALPRIO, usb_hub_thd, NULL);
+  
   /*
    * Initializes two serial-over-USB CDC drivers.
    */
---
+++ src/platforms/e-puck/2.0/platform.h
@@ -0,0 +1,9 @@
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
+
+#define CONFIGURED		1
+#define NOT_CONFIGURED	0
+
+#define DEBOUNCE_TIME_VBUS_DET_MS	100
+
+#endif /* __PLATFORM_H */
