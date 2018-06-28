0140ab3a68029277e44f097b02e271cf786d157e
Eliot Ferragni <eliot.ferragni@epfl.ch>
2018-06-28 10:17:13 +0200

    -Workaround to make the ADC work correctly -> need to sample on channel and one sample at a time => not use the CONT bit in CR2 and manage the conversion sequences manually in the callback

--- src/platforms/e-puck/2.0/battery_measurement.c
+++ src/platforms/e-puck/2.0/battery_measurement.c
@@ -4,21 +4,25 @@
 #include "power_button.h"
 
 #define ADC_NUM_CHANNELS		2	//Batt and VrefInt
-#define ADC_BATT_NUM_SAMPLES	32	//32 samples by channel
+#define ADC_BATT_NUM_SAMPLES	64	//32 samples by channel
+
+#define ADC_ONE_CHANNEL			1
+#define ADC_ONE_SAMPLE			1
 
 #define ADC_CHANNEL_BATT		8
 
+#define VOLTAGE_DROP			0.11f	//because of the diodes
 #define VOLTAGE_DIVIDER         (1.0f * RESISTOR_R2 / (RESISTOR_R1 + RESISTOR_R2))
 
 #define VREF                    3.0f //volt corresponds to the voltage on the VREF+ pin
-#define ADC_RESOLUTION          4096
+#define ADC_RESOLUTION          4095
 
 #define COEFF_ADC_TO_VOLT       ((1.0f * ADC_RESOLUTION * VOLTAGE_DIVIDER) / VREF) //conversion from adc value to voltage
 
 #define LOW_PASS_COEFF_A		0.8f
 #define LOW_PASS_COEFF_B		0.2f
 
-#define ADC_VALUE_VREFINT		1652	//value of the Vrefint (1.2V) measure when the uC is powered with 3V
+#define ADC_VALUE_VREFINT		1651.5f	//value of the Vrefint (1.2V) measure when the uC is powered with 3V
 
 //buffer filled by the ADC
 static adcsample_t batt_samples[ADC_NUM_CHANNELS * ADC_BATT_NUM_SAMPLES];
@@ -29,20 +33,63 @@ static float battery_voltage = 0;
 //Event source used to send events to other threads
 event_source_t battery_info_event;
 
+static BSEMAPHORE_DECL(measurement_ready, true);
+
+static void adcCb(ADCDriver *adcp, adcsample_t *buffer, size_t n);
+
 //groupe conversion config for the ADC
 static const ADCConversionGroup adcGroupConfig =  {
-	.circular = false, 
- 	.num_channels = ADC_NUM_CHANNELS,
- 	.end_cb = NULL,
+	.circular = true, 	//we need it to be circular to be able to relauche it simply by setting the SWSTART bit in CR2
+ 	.num_channels = ADC_ONE_CHANNEL,	//we need to do one conversion at a time -> num_channels need to be one
+ 	.end_cb = adcCb,
 	.error_cb = NULL,
 	.cr1 = 0,
-	.cr2 = ADC_CR2_SWSTART,
+	.cr2 = 0,
 	.smpr2 = ADC_SMPR2_SMP_AN8(ADC_SAMPLE_480),
 	.smpr1 = ADC_SMPR1_SMP_VREF(ADC_SAMPLE_480),
-	.sqr3 = ADC_SQR3_SQ1_N(ADC_CHANNEL_BATT) | ADC_SQR3_SQ2_N(ADC_CHANNEL_VREFINT),
+	.sqr3 = ADC_SQR3_SQ1_N(ADC_CHANNEL_BATT),// The first channel to be sampled is the battery
 	.sqr2 = 0,
 	.sqr1 = 0,
 };
+
+void ADCdoOneConversion(ADCDriver *adcp){
+	adcp->adc->CR2 |= ADC_CR2_SWSTART;
+}
+
+void ADCsetChannel(ADCDriver *adcp, uint8_t adc_channel){
+	adcp->adc->SQR3  = ADC_SQR3_SQ1_N(adc_channel);
+}
+
+//called after each single conversion
+//relauches a convesion and alternates the channel to sample.
+//we emulate a continuous conversion of 2 channels of ADC_BATT_NUM_SAMPLES samples
+//becasuse the adc has a random offset at the start of the uC when the CONT bit is set in CR2
+static void adcCb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
+
+	(void)adcp;
+	(void)n;
+	static uint8_t count = 0;
+	batt_samples[count] = buffer[0];
+	count++;
+	if(count < (ADC_NUM_CHANNELS * ADC_BATT_NUM_SAMPLES)){
+		if(count % 2){
+			//odd numbers => next sampling is for ADC_CHANNEL_VREFINT
+			ADCsetChannel(adcp, ADC_CHANNEL_VREFINT);
+		}else{
+			//even numbers => next sampling is for ADC_CHANNEL_BATT
+			ADCsetChannel(adcp, ADC_CHANNEL_BATT);
+		}
+		ADCdoOneConversion(adcp);
+	}else{
+		//we have finished. Reset the adc and counter for the first sample => ADC_CHANNEL_BATT
+		//and signal the end of conversion to the thread
+		count = 0;
+		ADCsetChannel(adcp, ADC_CHANNEL_BATT);
+		chSysLockFromISR();
+		chBSemSignalI(&measurement_ready);
+		chSysUnlockFromISR();
+	}
+}
 
 void batteryStateMachine(void){
 	//actual state
@@ -76,7 +123,7 @@ void batteryStateMachine(void){
 		if((time_state + CHANGE_STATE_TIME_MS) < chVTGetSystemTime()){
 			//we change the state
 			actual_state = new_state;
-
+			//we signal the new state
 			chEvtBroadcastFlags(&battery_info_event, actual_state);
 
 			time_state = chVTGetSystemTime();
@@ -90,7 +137,7 @@ void batteryStateMachine(void){
 	if(actual_state == MIN_VOLTAGE_FLAG){
 		if((time_battery_low + BATTERY_LOW_TIME_MS) < chVTGetSystemTime()){
 			//shutdown robot
-			chprintf((BaseSequentialStream *) &SD2,"robot shutdown !\n\n");
+			powerButtonTurnOnOff(POWER_OFF);
 		}
 	}else{
 		time_battery_low = chVTGetSystemTime();
@@ -105,21 +152,32 @@ static THD_FUNCTION(batt_thd, arg)
 
 	chRegSetThreadName("Battery measurement");
 
+	adcsample_t adc_measure[ADC_ONE_SAMPLE];	//we do one sample at a time
+
 	float battery_raw = 0;
 	float vrefint_raw = 0;
 
 	float battery_value = ADC_RESOLUTION;
-	float vref_gain_correction = 0;
+	float vref_gain_correction_value = 0;
 
 	systime_t time = 0;
 
+	//configures the ADC for the conversion but doesn't start it because the SWSTART is not set
+	adcStartConversion(&ADC_BATT, &adcGroupConfig, adc_measure, ADC_ONE_SAMPLE);
+
 	while(1){
 		time = chVTGetSystemTime();
+		//starts the conversion process.
+		//the adc callback will automatically relaunch the conversions until we have ADC_BATT_NUM_SAMPLES
+		//for ADC_CHANNEL_VREFINT and ADC_CHANNEL_BATT
+		ADCdoOneConversion(&ADC_BATT);
+		//the semaphore is released when all the conversions are finished
+		chBSemWait(&measurement_ready);
 
+		//we compute the battery state only if the robot is turned on
 		if(powerButtonGetPowerState() == POWER_ON){
-			//do one conversion and wait for it to finish -> no need for a callback
-			adcConvert(&ADC_BATT, &adcGroupConfig, batt_samples, ADC_BATT_NUM_SAMPLES);
-			
+
+			//we do the computation stuff
 			battery_raw = 0;
 			vrefint_raw = 0;
 			for(uint16_t i = 0 ; i < (ADC_NUM_CHANNELS * ADC_BATT_NUM_SAMPLES) ; i += ADC_NUM_CHANNELS){
@@ -130,19 +188,18 @@ static THD_FUNCTION(batt_thd, arg)
 			battery_raw /= ADC_BATT_NUM_SAMPLES;
 			vrefint_raw /= ADC_BATT_NUM_SAMPLES;
 
-			vref_gain_correction = ADC_VALUE_VREFINT/vrefint_raw;
+			vref_gain_correction_value = LOW_PASS_COEFF_A * vref_gain_correction_value
+									+ LOW_PASS_COEFF_B * (ADC_VALUE_VREFINT/vrefint_raw);
 
 			//low-pass filter
 			battery_value = LOW_PASS_COEFF_A * battery_value 
-							+ LOW_PASS_COEFF_B * battery_raw * vref_gain_correction;
+							+ LOW_PASS_COEFF_B * battery_raw * vref_gain_correction_value;
 
-			if(battery_value){
-				battery_voltage = battery_value / COEFF_ADC_TO_VOLT;
-			}
+			battery_voltage = (battery_value / COEFF_ADC_TO_VOLT) + VOLTAGE_DROP;
 
 
-			chprintf((BaseSequentialStream *) &SD2,"batt_raw =  %f , vref_raw = %f\n",battery_raw, vrefint_raw);
-			chprintf((BaseSequentialStream *) &SD2,"batt_filt =  %f , bat_volt_filt = %f\n\n",battery_value, battery_voltage);
+			chprintf((BaseSequentialStream *) &SDU2,"batt_raw =  %f , vref_raw = %f\n",battery_raw, vrefint_raw);
+			chprintf((BaseSequentialStream *) &SDU2,"batt_filt =  %f , bat_volt_filt = %f\n\n",battery_value, battery_voltage);
 
 			batteryStateMachine();
 		}
